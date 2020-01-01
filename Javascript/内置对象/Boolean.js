cabc72be62bf5c2f90ee341bd0a7325465d5b87d
agamgn <agamgn@163.com>
2020-01-01 11:23:06 +0800

    'boolean'

---
+++ Javascript/内置对象/Boolean.js
@@ -0,0 +1,15 @@
+// Boolean 对象表示两个值："true" 或 "false"。
+
+// constructor	返回对创建此对象的 Boolean 函数的引用
+console.log("1:constructor返回对创建此对象的 Boolean 函数的引用");
+// prototype	使您有能力向对象添加属性和方法。
+console.log("\n2:prototype使您有能力向对象添加属性和方法。");
+
+
+// toSource()	返回该对象的源代码。
+console.log("\n3:toSource()	返回该对象的源代码。");
+// toString()	把逻辑值转换为字符串，并返回结果。
+console.log("\n4:toString()	把逻辑值转换为字符串，并返回结果。");
+// valueOf()	返回 Boolean 对象的原始值。
+console.log("\n5:valueOf()返回 Boolean 对象的原始值。");
+
