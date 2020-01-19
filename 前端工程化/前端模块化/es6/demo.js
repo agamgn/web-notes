773285cedb2baa89dc1c5963782e321998a3de9b
agamgn <agamgn@163.com>
2020-01-19 18:34:03 +0800

    'es6'

---
+++ 前端工程化/前端模块化/es6/demo.js
@@ -0,0 +1,6 @@
+let name="agam";
+var sayName=name=>console.log(name);
+// export {name ,sayName};
+
+export default name;
+export default sayName;
---
+++ 前端工程化/前端模块化/es6/index.js
@@ -0,0 +1,3 @@
+import { name } from './demo';
+sayName("aga");
+console.log(name);
