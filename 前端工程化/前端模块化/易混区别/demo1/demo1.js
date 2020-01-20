101747400cf7b324f2048f3ed44cc60a42642f16
agamgn <agamgn@163.com>
2020-01-20 08:10:55 +0800

    'exports'

---
+++ 前端工程化/前端模块化/易混区别/demo1/demo1.js
@@ -0,0 +1,20 @@
+class Persion{
+    constructor(age){
+        this.age=age;
+    }
+    getName(){
+        return "agam";
+    }
+}
+function Persion2(){
+    console.log("agam2");
+}
+
+module.exports=Persion;
+// module.exports={
+//     Persion2
+// }
+// console.log(module.exports)
+// console.log(exports)
+
+
---
+++ 前端工程化/前端模块化/易混区别/demo1/index.js
@@ -0,0 +1,5 @@
+const persion=require("./demo1")
+let p =new persion();
+console.log(p.getName())
+
+persion.Persion2()
