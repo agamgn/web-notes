54840e084e3d07a7d0abf248e81f1a9a3f812681
agamgn <agamgn@163.com>
2020-03-25 12:49:44 +0800

    'class'

---
+++ TypeScript/base/class.ts
@@ -0,0 +1,71 @@
+class Animal{
+    public name;
+    private age;
+    protected year=888;
+    public constructor(name){
+        this.name=name;
+    }
+}
+
+class Cat extends Animal{
+    constructor(name){
+        super(name);
+        //console.log(this.age);//再累中是不被允许访问的
+        console.log(this.year);
+    }
+}
+let a=new Animal("ganin");
+console.log(a.name);
+a.name="tom";
+console.log(a.name);
+// console.log(a.age);
+
+
+// readonly只读属性关键字，只允许出现在属性声明或索引签名或构造函数中。
+class Animal2{
+    readonly name;
+    public constructor(name){
+        this.name=name;
+    }
+}
+
+let a1=new Animal2("jack");
+console.log(a1.name);
+// a1.name="TOM";
+
+
+
+// 抽象类是不允许被实例化,必须被子类实现
+abstract class Animal3{
+    // 不写类型的话直接猜测为any型
+    public name;
+    public constructor(name) {
+        this.name=name;
+    }
+    public abstract sayHi();
+}
+class Cat2 extends Animal3{
+    public sayHi() {
+       console.log("say hello");
+    }
+    
+}
+
+let cat=new Cat2("jacy");
+console.log(cat);
+
+
+// 类型的类型
+class Animal4{
+    public name:string;
+    constructor(name:string){
+        this.name=name;
+    }
+    sayHi():string{
+        return `My name is ${this.name}`;
+    }
+
+}
+
+let a4:Animal4=new Animal4("jack");
+console.log(a4.sayHi());
