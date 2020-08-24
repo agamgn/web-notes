3391c8ec3445a0c0a51431a41655691a4b167cb1
agamgn <agamgn@163.com>
2020-08-24 20:00:54 +0800

    day

--- Javascript/util/util3.js
+++ Javascript/util/util3.js
@@ -3,3 +3,9 @@
  */
 
  const RGBToHex=(r,g,b)=>((r<<16)+(g<<8)+b).toString(16).padStart(6,"0");
+
+
+ /**
+  * 返回两个日期之间以天为单位的差值
+  */
+ const getDayDiff = (date1, date2) => ((date2 - date1) / (1000 * 3600 * 24));
